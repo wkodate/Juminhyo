c34d58ec3b86a6f896a78556ea232677010b3306
wkodate <n9lfcth9@gmail.com>
2014-09-26 22:13:01 +0900

    initial commit

---
+++ classes/twitterapi.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import urllib
+import urllib2
+import re
+import json
+import twitter
+import sys
+sys.path.append("../")
+import secret
+
+class twitterapi:
+    def __init__(self):
+        self.charEncoding = "utf-8"
+        self.keyphraseUrl = "http://jlp.yahooapis.jp/KeyphraseService/V1/extract"
+        self.yahooApiAppId = secret.dict['APP_ID']
+        self.twitterApi = twitter.Api( 
+            consumer_key        = secret.dict['CONSUMER_KEY'],
+            consumer_secret     = secret.dict['CONSUMER_SECRET'],
+            access_token_key    = secret.dict['ACCESS_TOKEN'],
+            access_token_secret = secret.dict['ACCESS_TOKEN_SECRET']
+        )
+
+    def verifyCredentials(self):
+        return self.twitterApi.VerifyCredentials()
+
+    def getUserTimeline(self, name='wkodate', cnt=20):
+        return self.twitterApi.GetUserTimeline(screen_name=name, count=cnt)
+
+    def getSearch(self, tm, cnt=200):
+        return self.twitterApi.GetSearch(term=tm, count=cnt)
+
+    def printTweets(self, statuses):
+        for s in statuses:
+            print s.text.encode(self.charEncoding)
+
+    def getTweets(self, statuses):
+        tweets = []
+        for s in statuses:
+            tweets.append(s.text)
+        return tweets
+
+    def removeReply(self, text):
+        return re.sub(r'@\w+ ', '', text.encode(self.charEncoding))
+
+    def getFeaturedWords(self, statuses):
+        for s in statuses:
+            self.extractFeature(s.text)
+
+    def extractFeature(self, text):
+        normText = self.normalizeTweet(text)
+        sentence = urllib.quote_plus(normText)
+        query = "%s?appid=%s&output=%s&sentence=%s" % (self.keyphraseUrl, self.yahooApiAppId, "json", sentence)
+        c = urllib2.urlopen(query)
+        result = json.loads(c.read())
+        if len(result) == 0:
+            return
+        for k,v in sorted(result.items(), key=lambda x:x[1], reverse=True):
+            print k.encode(self.charEncoding)
+
+    def normalizeTweet(self, text):
+        # TODO remove www, RT and  etc.
+        # remove reply user's name
+        return re.sub(r'@\w+ ', '', text.encode(self.charEncoding))
+
+def list2String(list):
+    return " " . join(list)
