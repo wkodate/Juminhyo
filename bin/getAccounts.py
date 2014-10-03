57b0413892fffe3b5a89b195b77c6a422085b60d
wkodate <n9lfcth9@gmail.com>
2014-10-03 23:14:06 +0900

    modify script for scraper to get multiple web page

--- bin/getAccounts.py
+++ bin/getAccounts.py
@@ -4,10 +4,18 @@
 import sys
 sys.path.append("/Users/wkodate/Develop/Juminhyo/classes")
 import scraper
+import time
+
+BASE_URL = "http://www.talenttwit.com/tllink/tllink.php?mode=jump&id="
+SLEEP_TIME = 1
+END_PAGE_NUM = 2
 
 s = scraper.scraper()
-baseUrl = "http://www.talenttwit.com/tllink/tllink.php?mode=jump&id="
-html = s.getHtml(baseUrl)
-account = s.parseLink(html)
-print account[0], account[1]
+for i in range(1, END_PAGE_NUM):
+    html = s.getHtml(BASE_URL, i)
+    time.sleep(SLEEP_TIME)
+    account = s.parseTwLink(html)
+    if account is None:
+        continue
+    print '\t'.join([str(i) for i in account])
 
--- classes/scraper.py
+++ classes/scraper.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+sys.path.append("../")
 import urllib2
 from bs4 import *
 import re
-
-import sys
-sys.path.append("../")
 
 class scraper:
     def __init__(self):
         pass
 
-    def getHtml(self, url):
-        c = urllib2.urlopen(url+"9258")
+    def getHtml(self, url, param=None):
+        paramStr = ''
+        if (param is not None and isinstance(param, int)):
+            paramStr =  str(param)
+        c = urllib2.urlopen(url + paramStr)
         return c.read()
 
-    def parseLink(self, content):
+    def parseTwLink(self, content):
         soup = BeautifulSoup(content)
         pattern = re.compile(r'on Twitter$')
         anchor = soup.find(name='a', text=pattern)
+        if anchor is None:
+            return
         name = re.sub(pattern, '', anchor.renderContents()).strip()
         return (name, anchor['href'])
 
