b71f2618467003bbccc658a7aa32489eed4aa5d8
wkodate <n9lfcth9@gmail.com>
2014-10-04 21:33:46 +0900

    add column of user's profile text in tsv file

--- bin/getAccounts.py
+++ bin/getAccounts.py
@@ -4,18 +4,26 @@
 import sys
 sys.path.append("/Users/wkodate/Develop/Juminhyo/classes")
 import scraper
+import twitterapi
 import time
+import re
 
 BASE_URL = "http://www.talenttwit.com/tllink/tllink.php?mode=jump&id="
 SLEEP_TIME = 1
+START_PAGE_NUM = 1
 END_PAGE_NUM = 2
+LINE_SEPARATOR_PATTERN = re.compile(r'[\r\n|\r|\n]')
 
 s = scraper.scraper()
-for i in range(1, END_PAGE_NUM):
+for i in range(START_PAGE_NUM, END_PAGE_NUM):
     html = s.getHtml(BASE_URL, i)
     time.sleep(SLEEP_TIME)
     account = s.parseTwLink(html)
     if account is None:
         continue
+    t = twitterapi.twitterapi()
+    statuses = t.getUserTimeline(name=account[1], cnt=1)
+    desc = statuses[0].user.description.encode('utf-8')
+    account.append(re.sub(LINE_SEPARATOR_PATTERN, '', desc))
     print '\t'.join([str(i) for i in account])
 
--- classes/scraper.py
+++ classes/scraper.py
@@ -6,24 +6,50 @@ sys.path.append("../")
 import urllib2
 from bs4 import *
 import re
+import time
 
 class scraper:
     def __init__(self):
+        self.RETRY_COUNT_LIMIT = 2
+        self.SLEEP_TIME = 1
+        self._PATTERN = 'http://twitter.com/'
+        self.NAME_REPLACEMENT = re.compile(r'on Twitter$')
+        self.TWITTER_HOST = 'http://twitter.com/'
         pass
 
     def getHtml(self, url, param=None):
         paramStr = ''
+        # 受け取ったパラメータを文字列型に置換
         if (param is not None and isinstance(param, int)):
             paramStr =  str(param)
-        c = urllib2.urlopen(url + paramStr)
-        return c.read()
+        req = urllib2.Request(url + paramStr)
+        retryCount = 0
+        while True:
+            try:
+                if retryCount < self.RETRY_COUNT_LIMIT:
+                    response = urllib2.urlopen(req)
+                else:
+                    break
+            except urllib2.URLError, e:
+                if e.code == 200:
+                    break
+                else:
+                    # ステータスコードが200以外はリトライ　
+                    retryCount += 1
+                    time.sleep(self.SLEEP_TIME)
+            else:
+                return response.read()
+        return
 
     def parseTwLink(self, content):
         soup = BeautifulSoup(content)
-        pattern = re.compile(r'on Twitter$')
-        anchor = soup.find(name='a', text=pattern)
+        # 正規表現にマッチしたaタグを取得
+        anchor = soup.find(name='a', text=self.NAME_REPLACEMENT)
         if anchor is None:
             return
-        name = re.sub(pattern, '', anchor.renderContents()).strip()
-        return (name, anchor['href'])
+        # 人名を取得
+        name = re.sub(self.NAME_REPLACEMENT, '', anchor.renderContents()).strip()
+        # アカウント名を取得
+        screenName = re.sub(self.TWITTER_HOST, '', anchor['href'])
+        return [name, screenName]
 
