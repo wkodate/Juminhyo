2835e4b13c4c564b9c439144606ab0f5a59c06c6
wkodate <n9lfcth9@gmail.com>
2014-12-11 01:16:49 +0900

    add some output parameter from getFriendsLoco.py

--- bin/getFriendsLoco.py
+++ bin/getFriendsLoco.py
@@ -3,6 +3,7 @@
 
 import sys
 sys.path.append("/Users/wkodate/Develop/Juminhyo/classes")
+import re
 import twitterapi
 
 argvs = sys.argv
@@ -15,14 +16,25 @@ def printDebug():
 if argc != 2 : 
     printDebug()
 
+LINE_SEPARATOR_PATTERN = re.compile(r'[\r\n|\r|\n]')
 account = argvs[1]
 
 t=twitterapi.twitterapi()
-#print t.lookupFriendship(account)
 friends_statuses = t.getFriends(account);
 follower_statuses = t.getFollowers(account);
 for fr_s in friends_statuses:
     for fo_s in follower_statuses:
+        name = ""
+        timezone = ""
+        loco = ""
+        desc = ""
         if fr_s.id == fo_s.id:
-            print fr_s.screen_name
-            print fr_s.location
+            name = fr_s.screen_name
+            if fr_s.time_zone is not None:
+                timezone = fr_s.time_zone
+            if fr_s.location is not None:
+                loco = fr_s.location
+            if fr_s.description is not None:
+                desc = fr_s.description
+                desc = re.sub(LINE_SEPARATOR_PATTERN, '', desc)
+            print name+"\t"+timezone+"\t"+loco+"\t"+desc
